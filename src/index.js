3e33916acf05aa39d0b2637e8e928f37a822db4d
cuongdepzaivcl <becung742@gmail.com>
2022-09-17 09:08:49 +0700

    install prettier lint-staged husky
    # Conflicts:
    #	src/index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,39 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const { engine } = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-const route = require('./routes');
-
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-      app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-      // Khai báo cho app biết sử dụng handlebars engine
-      app.engine(
-          'hbs',
-          engine({
-              extname: '.hbs',
-          }),
-      );
+// Khai báo cho app biết sử dụng handlebars engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
 
 // set engine vào app
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 // set đường dẫn trỏ đến folder views
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set("views", path.join(__dirname, "resources/views"));
 
-// Routes init
-route(app);
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
